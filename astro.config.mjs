9acde3b3a41c9bcf1475e63e9179e177cc27338e
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-07-05 15:10:33 -0400

    optimized images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
+	integrations: [react(), tailwind(), astroImageTools],
 	vite: {
 		ssr: {
 			external: ['svgo'],
