03bc5e77c847aa4b2749afca4f9f4517ea58fcf7
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-07-05 15:20:02 -0400

    removed image optimization due to error in production I didn't know how to fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind(), astroImageTools],
+	integrations: [react(), tailwind()],
 	vite: {
 		ssr: {
 			external: ['svgo'],
