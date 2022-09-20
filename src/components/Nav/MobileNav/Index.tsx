9af409a6aedd11d295447cae5774791a18ebc4e6
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-20 15:39:46 -0400

    changed mobile nav styles

--- src/components/Nav/MobileNav/Index.tsx
+++ src/components/Nav/MobileNav/Index.tsx
@@ -9,18 +9,18 @@ import {
 	HiX,
 } from 'react-icons/hi/index.js';
 import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5/index.js';
-import { Popover, Transition } from '@headlessui/react';
 
 import Link from './Link/Index';
+import { Popover } from '@headlessui/react';
 import React from 'react';
 
 const MobileNav: React.FC = () => {
 	return (
-		<Popover className="relative sm:hidden z-40">
+		<Popover className="static sm:hidden z-40">
 			{({ open }) => (
 				<>
 					<Popover.Button
-						className="text-neutral-100 p-3 hover:bg-neutral-800 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-blue-400"
+						className="text-neutral-100 p-3 hover:bg-neutral-900 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-blue-400"
 						aria-label="menu"
 					>
 						{open ? (
@@ -30,45 +30,36 @@ const MobileNav: React.FC = () => {
 						)}
 					</Popover.Button>
 
-					<Transition
-						enter="transition duration-100 ease-out"
-						enterFrom="transform scale-95 opacity-0"
-						enterTo="transform scale-100 opacity-100"
-						leave="transition duration-75 ease-out"
-						leaveFrom="transform scale-100 opacity-100"
-						leaveTo="transform scale-95 opacity-0"
-					>
-						<Popover.Panel className="absolute z-40 right-0 bg-neutral-800 p-3 flex flex-col rounded-lg w-56 shadow-lg mt-3">
-							<Link to="/#" icon={HiHome}>
-								Home
-							</Link>
-							<Link to="/blog" icon={HiPencil}>
-								Blog
-							</Link>
-							<Link to="/#about" icon={HiUser}>
-								About
-							</Link>
-							<Link to="/#portfolio" icon={HiCollection}>
-								My Work
-							</Link>
-							<Link to="/#contact" icon={HiMail}>
-								Contact
-							</Link>
-							<div className="bg-neutral-700 h-px my-3" />
-							<Link
-								to="https://www.linkedin.com/in/raymondkneipp/"
-								icon={IoLogoLinkedin}
-							>
-								LinkedIn
-							</Link>
-							<Link to="https://github.com/raymondkneipp" icon={IoLogoGithub}>
-								GitHub
-							</Link>
-							<Link to="/resume.pdf" icon={HiDocumentText}>
-								Resume
-							</Link>
-						</Popover.Panel>
-					</Transition>
+					<Popover.Panel className="absolute z-40 left-0 right-0 top-full bg-neutral-900 p-3 m-3 rounded-lg shadow-lg grid grid-cols-2">
+						<Link to="/#" icon={HiHome}>
+							Home
+						</Link>
+						<Link to="/blog" icon={HiPencil}>
+							Blog
+						</Link>
+						<Link to="/#about" icon={HiUser}>
+							About
+						</Link>
+						<Link to="/#portfolio" icon={HiCollection}>
+							My Work
+						</Link>
+						<Link to="/#contact" icon={HiMail}>
+							Contact
+						</Link>
+						<div className="bg-neutral-800 h-px my-3 col-span-full" />
+						<Link
+							to="https://www.linkedin.com/in/raymondkneipp/"
+							icon={IoLogoLinkedin}
+						>
+							LinkedIn
+						</Link>
+						<Link to="https://github.com/raymondkneipp" icon={IoLogoGithub}>
+							GitHub
+						</Link>
+						<Link to="/resume.pdf" icon={HiDocumentText}>
+							Resume
+						</Link>
+					</Popover.Panel>
 				</>
 			)}
 		</Popover>
--- src/components/Nav/MobileNav/Link/Index.tsx
+++ src/components/Nav/MobileNav/Link/Index.tsx
@@ -13,7 +13,9 @@ const Link: React.FC<Props> = ({ to, children, icon }) => {
 			href={to}
 			className="text-neutral-100 px-6 py-3 flex items-center hover:bg-neutral-700 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-blue-400"
 		>
-			{React.createElement(icon, { size: 18, className: 'mr-3' })}
+			<span className="text-blue-400">
+				{React.createElement(icon, { size: 18, className: 'mr-3' })}
+			</span>
 			{children}
 		</a>
 	);
