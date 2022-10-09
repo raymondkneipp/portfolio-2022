cca7cd9f029898295d431d00fcf8fdfe662ffe11
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-10-09 19:34:09 -0400

    updated uc time span in timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -92,7 +92,7 @@ const events: Event[] = [
 		year: 2017,
 		month: 12,
 		content:
-			"I received 27 credits by taking dual credit courses in my last year and a half of high school, focusing on software development, and I made the dean's list.",
+			"I received 27 credits by taking dual credit courses in my last year of high school, focusing on software development, and I made the dean's list.",
 	},
 	{
 		title: 'Angular',
