f7fbd469748c10697127c7ce6a1d55c181c52460
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-10-09 22:41:06 -0400

    fixed links in mobile nav

--- src/components/Nav/MobileNav/Index.tsx
+++ src/components/Nav/MobileNav/Index.tsx
@@ -17,7 +17,7 @@ import React from 'react';
 const MobileNav: React.FC = () => {
 	return (
 		<Popover className="static sm:hidden z-40">
-			{({ open }) => (
+			{({ open, close }) => (
 				<>
 					<Popover.Button
 						className="z-50 relative text-neutral-100 p-3 hover:bg-neutral-900 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-blue-400"
@@ -33,32 +33,48 @@ const MobileNav: React.FC = () => {
 					<Popover.Overlay className="fixed inset-0 bg-black opacity-80 z-10" />
 
 					<Popover.Panel className="absolute z-40 left-0 right-0 top-full bg-neutral-900 p-3 m-3 rounded-lg shadow-lg grid grid-cols-2">
-						<Link to="/#" icon={HiHome}>
+						<Link to="/#" icon={HiHome} onClick={() => close()}>
 							Home
 						</Link>
-						<Link to="/blog" icon={HiPencil}>
+						<Link to="/blog" icon={HiPencil} onClick={() => close()}>
 							Blog
 						</Link>
-						<Link to="/about" icon={HiUser}>
+						<Link to="/about" icon={HiUser} onClick={() => close()}>
 							About
 						</Link>
-						<Link to="/#website-projects" icon={HiCollection}>
+						<Link
+							to="/#website-projects"
+							icon={HiCollection}
+							onClick={() => close()}
+						>
 							Projects
 						</Link>
-						<Link to="/#contact-me" icon={HiMail}>
+						<Link to="/#contact-me" icon={HiMail} onClick={() => close()}>
 							Contact
 						</Link>
 						<div className="bg-neutral-800 h-px my-3 col-span-full" />
 						<Link
 							to="https://www.linkedin.com/in/raymondkneipp/"
+							onClick={() => close()}
 							icon={IoLogoLinkedin}
+							target="_blank"
 						>
 							LinkedIn
 						</Link>
-						<Link to="https://github.com/raymondkneipp" icon={IoLogoGithub}>
+						<Link
+							to="https://github.com/raymondkneipp"
+							icon={IoLogoGithub}
+							onClick={() => close()}
+							target="_blank"
+						>
 							GitHub
 						</Link>
-						<Link to="/resume.pdf" icon={HiDocumentText}>
+						<Link
+							to="/20220930-resume.pdf"
+							icon={HiDocumentText}
+							onClick={() => close()}
+							target="_blank"
+						>
 							Resume
 						</Link>
 					</Popover.Panel>
--- src/components/Nav/MobileNav/Link/Index.tsx
+++ src/components/Nav/MobileNav/Link/Index.tsx
@@ -5,13 +5,23 @@ type Props = {
 	to: string;
 	children: string;
 	icon: IconType;
+	onClick?: () => void;
+	target?: string;
 };
 
-const Link: React.FC<Props> = ({ to, children, icon }) => {
+const Link: React.FC<Props> = ({
+	to,
+	children,
+	icon,
+	onClick,
+	target = '_self',
+}) => {
 	return (
 		<a
+			onClick={onClick}
 			href={to}
 			className="text-neutral-100 px-6 py-3 flex items-center hover:bg-neutral-700 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-blue-400"
+			target={target}
 		>
 			<span className="text-blue-400">
 				{React.createElement(icon, { size: 18, className: 'mr-3' })}
